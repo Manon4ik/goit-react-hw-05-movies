9fe6d2b343fd97b203ad8e034e3cb7d0c2274522
Manon4ik <manon4ik@gmail.com>
2025-01-27 21:58:14 +0000

    Deploying to gh-pages from @ Manon4ik/goit-react-hw-05-movies@7947b01abfedaeb09421957fd73312e49582584a 🚀

---
+++ static/js/notFound.178a1fa7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[449],{922:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Not Found"})})}}}]);
+//# sourceMappingURL=notFound.178a1fa7.chunk.js.map
